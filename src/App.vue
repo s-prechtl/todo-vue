25b37fb4d52616d66fce63fc675bb3940d3081a1
s-prechtl <sprechtl@htl-steyr.ac.at>
2022-06-01 11:36:38 +0200

    komisch routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <todo-listing></todo-listing>
+  <router-view></router-view>
 </template>
 
 <script>
-import todoListing from "@/components/todoListing";
 export default {
   name: 'App',
   components: {
-    todoListing
+
   }
 }
+</script>
+
+<script>
+import todoListing from "@/components/todoListing";
+const listing = todoListing
+const About = { template: '<div>About</div>' }
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: listing },
+  { path: '/about', component: About },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = Vue.createApp({})
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
+
+// Now the app has started!
 </script>
 
 <style>
